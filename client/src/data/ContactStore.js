3e886fd5ce7ea85e6ba10b2109acd7a9375602f9
Michael Robertson <mbr.robertson@gmail.com>
2020-07-25 17:06:56 +1200

    Simple interface for keeping track of a users contacts

---
+++ client/src/data/ContactStore.js
@@ -0,0 +1,38 @@
+const DEFUALT_CONTACTS = {
+    "0x0bbd91c075433599094fb973e55235d239d22f8b": {
+        name: "John Doe"
+    },
+    "0xa5d844e32288304184efdd8ed45896b4d7ca853a": {
+        name: "Jane Sue"
+    }
+}
+
+export const getContacts = () => {
+    let contacts = JSON.parse(window.localStorage.getItem("contacts"))
+    if (contacts === null) {
+        contacts = DEFUALT_CONTACTS
+        window.localStorage.setItem("contacts", JSON.stringify(contacts))
+    }
+    return contacts
+}
+
+export const addContact = (address, data) => {
+    let contacts = getContacts();
+    delete contacts[address];
+    window.localStorage.setItem("contacts", JSON.stringify(contacts))
+}
+
+export const deleteContact = (address) => {
+    let contacts = getContacts();
+    delete contacts[address];
+    window.localStorage.setItem("contacts", JSON.stringify(contacts))
+}
+
+export const addressToName = (address) => {
+    let contacts = getContacts()
+    if (contacts[address] && contacts[address].name) {
+        return contacts[address].name
+    } else {
+        return address
+    }
+}
